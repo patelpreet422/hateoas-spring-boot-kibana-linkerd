1e3ec17ad8b0db822bcdd3f04da700a25ef3d261
Preet Patel <patelpreet422@gmail.com>
2023-04-27 00:26:21 +0530

    add hateoas to GET /users

--- src/main/java/com/example/userapi/controller/UserController.java
+++ src/main/java/com/example/userapi/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.userapi.controller;
 import com.example.userapi.model.User;
 import com.example.userapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/users")
@@ -19,9 +24,16 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
-    public List<User> getUsers() {
-        return userRepository.findAll();
+    public ResponseEntity<CollectionModel<EntityModel<User>>> getUsers() {
+        List<EntityModel<User>> users = userRepository.findAll().stream()
+                .map(user -> EntityModel.of(user,
+                        linkTo(methodOn(UserController.class).getUser(user.getId())).withSelfRel(),
+                        linkTo(methodOn(UserController.class).getUsers()).withRel("users")))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(CollectionModel.of(users,
+                linkTo(methodOn(UserController.class).getUsers()).withSelfRel()));
     }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
